0c64b065706dd7288369cfb0768c4056746afca4
Matt Anderson <42154938+matoszz@users.noreply.github.com>
2023-11-25 22:23:42 -0700

    add utils package (#132)
    * add utils package
    
    Signed-off-by: Matthew Anderson <42154938+matoszz@users.noreply.github.com>
    
    * linter
    
    Signed-off-by: Matthew Anderson <42154938+matoszz@users.noreply.github.com>
    
    * Update internal/utils/sqlite/sqlite.go
    
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>
    
    * Update internal/utils/sqlite/sqlite.go
    
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>
    
    * Update internal/utils/ulids/ulids.go
    
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>
    
    * Update internal/utils/ulids/ulids.go
    
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>
    
    * Update internal/utils/ulids/ulids.go
    
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>
    
    * linter
    
    Signed-off-by: Matthew Anderson <42154938+matoszz@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Matthew Anderson <42154938+matoszz@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -97,6 +97,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid/v2 v2.1.0
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
---
+++ internal/utils/sqlite/doc.go
@@ -0,0 +1,2 @@
+// Package sqlite implements a connect hook around the sqlite3 driver so that the underlying connection can be fetched from the driver for more advanced operations such as backups. See: https://github.com/mattn/go-sqlite3/blob/master/_example/hook/hook.go. To use make sure you import this package so that the init code registers the driver: import _ github.com/datumforge/datum/internal/utils/sqlite - Then you can use sql.Open in the same way you would with sqlite3: sql.Open("datum_sqlite3", "path/to/database.db")
+package sqlite
